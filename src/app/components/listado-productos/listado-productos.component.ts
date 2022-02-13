2f4aeeedc1023fca7026d87f6025d463f40c7273
Diego Anacona <diefer123anacona@gmail.com>
2022-02-13 18:33:23 -0500

    Agregar

--- src/app/components/listado-productos/listado-productos.component.ts
+++ src/app/components/listado-productos/listado-productos.component.ts
@@ -34,24 +34,11 @@ export class ListadoProductosComponent implements OnInit {
     });
   }
   eliminarProducto(id: string) {
-    Swal.fire({
-      title: '¿Estás seguro?',
-      text: "¡No podrás revertir esto!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si,borrar!'
-
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Eliminado!',
-          'Su archivo ha sido eliminado.',
-
-        )
-      }
-    })
+    Swal.fire(
+      'Canción eliminada!',
+      'Se elimino correctamente!',
+      'success'
+    )
 
     this._productoService.eliminarProductos(id).then(() => {
       /*console.log('producto eliminado con exito');*/
